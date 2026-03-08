dd61d1c6b40b960b9c0c47789443c17dc0d26b51
yogesh-gohil <yogeshgohil1611@gmail.com>
2026-03-08 23:48:42 +0530

    fix duplicate route name

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,11 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('api.login');
+Route::post('/register', [AuthController::class, 'register'])->name('api.register');
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('api.logout');
 
     Route::get('/me', function (Request $request) {
         return $request->user();
